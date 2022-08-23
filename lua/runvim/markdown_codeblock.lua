965fac757715ef7c250d0df0c0fe419e90871b3c
lukaaa-m <luka@lukamanitta.com>
2022-08-23 21:19:13 +1000

    feat: iterate over matches instead of captures (to encapsulate both language and content in same loop)

--- lua/runvim/markdown_codeblock.lua
+++ lua/runvim/markdown_codeblock.lua
@@ -22,12 +22,22 @@ U.get_codeblock_under_cursor = function()
     local bufnr = vim.api.nvim_get_current_buf()
     local root = get_root(bufnr)
 
-    for id, node in code_block:iter_captures(root, bufnr, 0, -1) do
-        local language = code_block.captures[id]
-        local start_row, _, end_row, _ = node:range()
-        local linenr = vim.api.nvim_win_get_cursor(0)[1]
-        if start_row <= linenr and linenr <= end_row then
-            vim.notify("Inside a code block of language" .. language)
+    for pattern, match in code_block:iter_captures(root, bufnr, 0, -1) do
+        local language
+        local content
+        for id, node in pairs(match) do
+            local capture_name = code_block.captures[id]
+            if capture_name == "language" then
+                language = node:get_node_text(bufnr)
+            elseif capture_name == "content" then
+                content = node:get_node_text(bufnr)
+
+                local start_row, _, end_row, _ = node:range()
+                local linenr = vim.api.nvim_win_get_cursor(0)[1]
+                if start_row <= linenr and linenr <= end_row then
+                    vim.notify("Inside a code block of language " .. language)
+                end
+            end
         end
     end
 end
