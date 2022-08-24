0ebeb9894767f57c7222053d7597bba46994196d
lukaaa-m <luka@lukamanitta.com>
2022-08-24 10:40:54 +1000

    feat: moved usercmd into setup command

--- lua/runvim/init.lua
+++ lua/runvim/init.lua
@@ -1,11 +1,13 @@
 local M = {}
+local options = {}
 
 function M.setup(opts)
     require("runvim.config").set_options(opts)
-end
+    options = require("runvim.config").options
 
-vim.api.nvim_create_user_command("TestCbQuery", function()
-    require("markdown_codeblock").get_codeblock_under_cursor()
-end, {})
+    vim.api.nvim_create_user_command("TestCbQuery", function()
+        require("runvim.markdown_codeblock").get_codeblock_under_cursor()
+    end, {})
+end
 
 return M
