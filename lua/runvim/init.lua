8fdc24650faeb897d6c9bc1fa03131e44ac26b40
lukaaa-m <luka@lukamanitta.com>
2022-08-24 10:36:31 +1000

    feat: add usercmd to test (working on setting up local testing...)

--- lua/runvim/init.lua
+++ lua/runvim/init.lua
@@ -4,4 +4,8 @@ function M.setup(opts)
     require("runvim.config").set_options(opts)
 end
 
+vim.api.nvim_create_user_command("TestCbQuery", function()
+    require("markdown_codeblock").get_codeblock_under_cursor()
+end, {})
+
 return M
